8c0a0b8494514ed44a30c12e981fd60d5300dd53
Elif PİRGAİP <elifpirgaip27@gmail.com>
2021-02-13 14:57:54 +0300

    2. gün 6. ödev

---
+++ BasitETicaretOrn/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasitETicaretOrn
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunKategori { get; set; }
+        public int UrunNo { get; set; }
+    }
+}
---
+++ BasitETicaretOrn/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasitETicaretOrn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.UrunKategori = "Giyim";
+            product1.UrunNo = 12345;
+            product1.UrunAdi="Elbise";
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.UrunKategori = "Teknolojik";
+            product2.UrunNo = 1234567;
+            product2.UrunAdi = "JBL Kulaklık";
+
+            Product[] products = new Product[] { product1, product2 };
+            foreach (Product product in products)
+            {
+                Console.WriteLine("Ürün Id : " + product.Id + " / Ürün Kategori : " + product.UrunKategori + " / Ürün No : " + product.UrunNo + " / Ürün Adı : " + product.UrunAdi);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}

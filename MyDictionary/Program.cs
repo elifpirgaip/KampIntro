5069ff583dc44840e0d207150845af5878ff1ab6
Elif PİRGAİP <elifpirgaip27@gmail.com>
2021-02-13 20:41:22 +0300

    4. gün 5. ödev

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(34, "İstanbul");
+            dictionary.Add(35, "İzmir");
+
+            foreach (var city in dictionary)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}

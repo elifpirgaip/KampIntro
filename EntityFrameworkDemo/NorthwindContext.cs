0a99915384b748faa26ae24e681f7f4add34f89a
Elif PİRGAİP <elifpirgaip27@gmail.com>
2021-02-27 22:47:25 +0300

    27.02.2021 - Entity

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        // override : üzerine yazmak / ezmek
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        // Elimizdeki "Product" classımız ile veritabanındaki "Products"ı ilişkilendirdik aşağıda.
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ADO.NET : veri tabanımıza bağlanmamızı, kontrol etmemizi ve sorgu yapmamızı sağlayan bir kütüphanedir.
+            // Bu kütüphane sayesinde verilerimizi ekleyebilir, silebilir, güncelleyebiliriz. 
+            // Entity Framework -- Bir ORM - Object Relational Mapping --> Nesnelerin veritabanı ile bağdaştırılması. Core'dan sonraki versiyonunun kullanılması önerilir.
+            // NHibernate :
+            // Dapper : ADO.NET'in kolaylaştırılmış versiyonudur.
+
+            // GetAll();
+            GetProductsByCategory(1);
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            var result = northwindContext.Products.Where(p=> p.CategoryId==categoryId);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
